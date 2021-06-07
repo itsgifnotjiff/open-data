d6adfe49fd1d349e5fefd01f853994a8dc44f507
Yordan Radev <50274281+itsgifnotjiff@users.noreply.github.com>
2021-06-07 14:28:44 -0500

    Deployed 6afb980 with MkDocs version: 1.1.2

---
+++ js/radar.js
@@ -0,0 +1,116 @@
+const parser = new DOMParser();
+
+async function getRadarStartEndTime() {
+  let response = await fetch('https://geo.weather.gc.ca/geomet/?lang=en&service=WMS&request=GetCapabilities&version=1.3.0&LAYERS=RADAR_1KM_RSNO')
+  let data = await response.text().then(
+    data => {
+      let xml = parser.parseFromString(data, 'text/xml')
+      let [start, end] = xml.getElementsByTagName('Dimension')[0].innerHTML.split('/')
+      let default_ = xml.getElementsByTagName('Dimension')[0].getAttribute('default')
+      return [start, end, default_]
+    }
+  )
+  return [new Date(data[0]), new Date(data[1]), new Date(data[2])]
+}
+
+let frameRate = 1.0; // frames per second
+let animationId = null;
+let startTime = null
+let endTime = null
+let defaultTime = null
+let current_time = null;
+
+let layers = [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    new ol.layer.Image({
+      source: new ol.source.ImageWMS({
+        format: 'image/png',
+        url: 'https://geo.weather.gc.ca/geomet/',
+        params: {'LAYERS': 'RADAR_1KM_RSNO', 'TILED': true},
+        crossOrigin: 'Anonymous'
+      })
+    }),
+    new ol.layer.Image({
+      source: new ol.source.ImageWMS({
+        format: 'image/png',
+        url: 'https://geo.weather.gc.ca/geomet/',
+        params: {'LAYERS': 'RADAR_COVERAGE_RSNO.INV', 'TILED': true},
+        transition: 0,
+        crossOrigin: 'Anonymous'
+      })
+    })
+  ]
+
+let map = new ol.Map({
+  target: 'map',
+  layers: layers,
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-97, 57]),
+    zoom: 3
+  })
+});
+
+function updateInfo(current_time) {
+  let el = document.getElementById('info');
+  el.innerHTML = `Time / Heure (UTC): ${current_time.toISOString()}`
+}
+
+function setTime() {
+  current_time = current_time
+    if (current_time === null) {
+      current_time = defaultTime;
+    } else if (current_time >= endTime) {
+      current_time = startTime
+    } else {
+      current_time = new Date(current_time.setUTCMinutes(current_time.getUTCMinutes() + 10));
+    }
+    layers[1].getSource().updateParams({'TIME': current_time.toISOString().split('.')[0]+"Z"});
+    layers[2].getSource().updateParams({'TIME': current_time.toISOString().split('.')[0]+"Z"});
+    updateInfo(current_time)
+}
+
+getRadarStartEndTime().then(data => {
+    startTime = data[0]
+    endTime = data[1]
+    defaultTime = data[2]
+    setTime();
+})
+
+
+let stop = function() {
+  if (animationId !== null) {
+    window.clearInterval(animationId);
+    animationId = null;
+  }
+};
+
+let exportMapFunction = function() {
+    map.once('postcompose', function(event) {
+      var canvas = event.context.canvas;
+      if (navigator.msSaveBlob) {
+        navigator.msSaveBlob(canvas.msToBlob(), 'map.png');
+      } else {
+        canvas.toBlob(function(blob) {
+          saveAs(blob, 'map.png');
+        });
+      }
+    });
+    map.renderSync();
+};
+
+
+let play = function() {
+  stop();
+  animationId = window.setInterval(setTime, 1000 / frameRate);
+};
+
+let startButton = document.getElementById('play');
+startButton.addEventListener('click', play, false);
+
+let stopButton = document.getElementById('pause');
+stopButton.addEventListener('click', stop, false);
+
+let exportButton = document.getElementById('exportmap');
+exportButton.addEventListener('click', exportMapFunction, false);
